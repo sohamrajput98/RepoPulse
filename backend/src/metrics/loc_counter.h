9b036c92db70492df2d3d0f4d207e20bc888a838
Soham <sohamrw098@gmail.com>
2026-03-15 16:48:55 +0000

    backend: update LOC counter header declarations

--- backend/src/metrics/loc_counter.h
+++ backend/src/metrics/loc_counter.h
@@ -7,11 +7,10 @@ struct LocResult {
     int code     = 0;
     int comments = 0;
     int blank    = 0;
+    double commentRatio = 0.0;
 };
 
 class LocCounter {
 public:
-    // EXTEND THIS FUNCTION: handle block comments (/* ... */ spanning multiple
-    // lines) using a state machine to correctly classify each line category
     LocResult count(const std::vector<std::string>& lines) const;
 };
