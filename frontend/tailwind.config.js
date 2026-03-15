69f6d86c331d176c370a6d604fec81658bdaa1f1
Soham <sohamrw098@gmail.com>
2026-03-15 16:48:55 +0000

    frontend: update Tailwind configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./src/**/*.{jsx,js}'],
+  theme: {
+    extend: {
+      colors: {
+        excellent: '#22c55e',
+        good:      '#84cc16',
+        fair:      '#f59e0b',
+        poor:      '#ef4444',
+      }
+    }
+  },
+  plugins: []
+}
